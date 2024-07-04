c971c46e7104e6ecf465ddf3adf41c7571553b4f
u2201142 <u2201142@rajagiri.edu.in>
2024-07-04 14:30:48 +0530

    Add files via upload

---
+++ sizzling_bombul_wluff1.ino
@@ -0,0 +1,21 @@
+int buttonPin=2;
+int led=12;
+int buttonstate=0;
+
+void setup()
+{
+  pinMode(led,OUTPUT);
+  pinMode(buttonPin,INPUT);
+}
+ void loop()
+ {
+   buttonstate = digitalRead(buttonPin);
+   if (buttonstate == HIGH);     
+  {
+    digitalWrite(led,HIGH);
+  } 
+   else 
+  {
+    digitalWrite(led,LOW);
+  }
+ }
